86b2d958737a28f1c959dceafbb618605ff655eb
Virusnest <kai.maloney.au@gmail.com>
2024-11-29 10:16:14 +1100

    Clean Up code and comments

--- main.py
+++ main.py
@@ -8,38 +8,45 @@ class Plan:
 def loadfile(filename):
     with open(filename) as file:
         for line in file:
-            # split the line into a list of strings
+            # ignore comments
             if line[0] == "#":
                 continue
+            # split the line into a list of strings
             parts = line.split(" ")
+
             # create a new plan object
             plan = Plan()
-            # assign the values from the list to the object
             plan.Name = parts[0]
             plan.UpfrountCost = int(parts[1])
             plan.MonthlyCost = int(parts[2])
+
             # add the plan object to the list
             list.append(plan)
 
 def printlist():
+    print("Name | Upfront | Monthly")
     for plan in list:
-        print(plan.Name, plan.UpfrountCost, plan.MonthlyCost)
-
+        print(plan.Name,"$"+str(plan.UpfrountCost), "$"+str(plan.MonthlyCost))
 
 def calculate():
     totals = []
+    # calculate the total cost for each plan
     for plan in list:
         total = plan.UpfrountCost + (plan.MonthlyCost * time)
         totals.append(total)
-    # find largest and smallest totoal then print all plans hilighting the best and worst
+    # find largest and smallest total
     best = min(totals)
     worst = max(totals)
+    # print the best and worst plans
+    print("Name | Upfront | Monthly")
+
     for i in range(len(list)):
         if totals[i] == best:
             print("Best Plan: ", end='')
         elif totals[i] == worst:
             print("Worst Plan: ",end='')
         print(list[i].Name,"$"+ str(totals[i]))
+
 
 loadfile("plans.txt")
 printlist()
